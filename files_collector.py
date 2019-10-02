a985ca3321813ba211a75310a228e038718e9790
amirl <amir4895@gmail.com>
2019-10-03 02:50:02 +0300

    files collector class - first and basic code

---
+++ files_collector.py
@@ -0,0 +1,33 @@
+from pathlib import Path
+from file_word_counter import FileWordCounter
+from os import path as os_path
+
+
+class FileCollector:
+
+    def __init__(self, paths):
+        """
+        :param paths: list of paths to txt files and or directories
+        """
+        self.paths = paths
+        self.paths_as_set = set()
+        self.invalid_paths = []
+        self.mapper = {}
+        self.populate_mapper()
+
+    def validate_input(self):
+        pass
+
+    def populate_mapper(self):
+        """
+        function traverse all paths and populate the mapper
+        """
+        for arg_path in self.paths:
+            if os_path.exists(arg_path):
+                if os_path.isfile(arg_path):
+                    self.paths_as_set.add(Path(arg_path))
+                if os_path.isdir(arg_path):
+                    for file_path in Path(arg_path).glob('**/*.txt'):
+                        self.paths_as_set.add(file_path)
+        for path in self.paths_as_set:
+            self.mapper[path._str] = FileWordCounter(file_path=path)
