c869e1b1d3a51d994291caa0e261c31b825b9f9c
amirl <amir4895@gmail.com>
2019-10-03 03:00:10 +0300

    file_word_counter class first and basic commit

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import argparse
+from files_collector import FileCollector
+from concurrent.futures import ProcessPoolExecutor
+from collections import Counter
 
 
 def main():
@@ -8,7 +11,17 @@ def main():
     parser.add_argument('-P', '--paths', nargs='+', help='Paths to text files/folders',
                         required=True)
     args = parser.parse_args()
-    print(args)
+
+    file_collector = FileCollector(args.paths)
+
+    with ProcessPoolExecutor() as executor:
+        for file_word_count in file_collector.mapper.values():
+            executor.submit(file_word_count.analyze_file)
+
+    c = Counter()
+    for file_word_count in file_collector.mapper.values():
+        c += file_word_count.words_counter
+    print(f"The {args.n_max}  most common words are {c.most_common(args.n_max)} ")
 
 
 if __name__ == "__main__":
