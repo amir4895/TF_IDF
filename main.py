8520597684f078a1b97362ad9835f5ff4734e414
amirl <amir4895@gmail.com>
2019-10-04 18:10:54 +0300

    creating TF_IDF class keeping main function short more oop adding log

--- main.py
+++ main.py
@@ -1,27 +1,26 @@
 import argparse
-from files_collector import FileCollector
-from concurrent.futures import ProcessPoolExecutor
-from collections import Counter
+from validation_args_functions import check_not_neg, validate_paths_arg
+from tf_idf import TFIDF
 
 
 def main():
-    parser = argparse.ArgumentParser(description='TF_IDF arg parser.')
-    parser.add_argument('-N', '--n_max',  type=int, action="store", help='N words with maximal of occurrences',
-                        required=True)
-    parser.add_argument('-P', '--paths', nargs='+', help='Paths to text files/folders',
-                        required=True)
+
+    parser = argparse.ArgumentParser(description='TF_IDF arg parser.', add_help=False)
+    parser.add_argument('--max­words', dest="max_words", type=check_not_neg, action="store",
+                        help='N words with maximal of occurrences', required=True)
+
+    parser.add_argument(dest="paths", nargs='+',  type=validate_paths_arg,
+                        help='Paths to text files/folders')
+
+    parser.add_argument('-h', '--help', action='help', default=argparse.SUPPRESS,
+                        help='Show this help message and exit.')
+
     args = parser.parse_args()
 
-    file_collector = FileCollector(args.paths)
-
-    with ProcessPoolExecutor() as executor:
-        for file_word_count in file_collector.mapper.values():
-            executor.submit(file_word_count.analyze_file)
-
-    c = Counter()
-    for file_word_count in file_collector.mapper.values():
-        c += file_word_count.words_counter
-    print(f"The {args.n_max}  most common words are {c.most_common(args.n_max)}")
+    tfidf_instance = TFIDF(paths=args.paths)
+    tfidf_instance.parse_files()
+    tfidf_instance.join_counters()
+    tfidf_instance.get_n_max_common(args.max_words)
 
 
 if __name__ == "__main__":
---
+++ tf_idf.py
@@ -0,0 +1,47 @@
+from files_collector import FileCollector
+from concurrent.futures import ProcessPoolExecutor
+from collections import Counter
+from tf_idf_logger import LOG
+
+
+class TFIDF:
+
+    def __init__(self, paths: list):
+
+        """init the tf idf object and """
+        self.global_counter = None
+        self.logger = LOG
+        self.collector = FileCollector(paths, logger=self.logger)
+        self.collector.populate_mapper()
+
+    def parse_files(self):
+        with ProcessPoolExecutor() as executor:
+            for file_word_count in self.collector.mapper:
+                self.collector.mapper[file_word_count] = executor.submit(file_word_count.analyze_file)
+        for word_counter, process in self.collector.mapper.items():
+            self.collector.mapper[word_counter] = process.result()
+
+    def join_counters(self, new_counters=None):
+        """
+        calc the total freq of all words
+        :param new_counters: if new files added calc only the diff
+        """
+        if not new_counters:
+            self.global_counter = Counter()
+            for file_word_count in self.collector.mapper.values():
+                self.global_counter += file_word_count
+        else:
+            for counter in new_counters:
+                self.global_counter += counter
+
+    def get_n_max_common(self, max_words: int):
+        """
+        :param max_words: number of common words to print and return
+        :return: list of tuples (str: counter)
+        """
+        print(f"Maximum {max_words} words:")
+        most_common = self.global_counter.most_common(max_words)
+        for common in most_common:
+            print(f"Word ‘{common[0]}’ occurred {common[1]} times")
+        self.logger.info(f"Get N Max Common returned:\n {most_common} ")
+        return most_common
